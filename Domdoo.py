4c8a85cb80020e74b166cdc48a2f314b5a5e941f
Vishal <vishal.saroj18@hotmail.com>
2021-07-11 10:49:41 +0530

    added TakeCommand

--- Domdoo.py
+++ Domdoo.py
@@ -1,12 +1,12 @@
 import pyttsx3
 import datetime
+import speech_recognition as sr
 
 
 engine = pyttsx3.init('sapi5')
 
 voices = engine.getProperty('voices')
 
-print(voices[0].id)
 
 engine.setProperty('voice',voices[0].id)
 
@@ -23,7 +23,30 @@ def wishMe():
         speak("Good afternoon sir")
     else:
         speak("Good evening sir")
+        
     speak("i am Domdoo 1 point o")
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language = 'en-in')
+        print("User said : " , query )
+
+    except Exception as e:
+        # print(e)
+
+        print("say that again please...")
+
+        return"None"
+    return query
+
 
 if __name__ == "__main__" :
     wishMe()
+    takeCommand()
